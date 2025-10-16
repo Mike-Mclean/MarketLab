e94ff1796973b214b55e148b0673bcae5db5a78d
Mike-Mclean <mmcle009@gmail.com>
2025-10-16 16:18:42 -0400

    Updated CSS and text pop-ups on Registration page

--- stockApp - ui/src/pages/RegisterPage.js
+++ stockApp - ui/src/pages/RegisterPage.js
@@ -78,7 +78,7 @@ const RegisterPage = () => {
 
 
   return (
-    <section className="h-screen flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 bg-orange-100 ">
+    <section className="h-screen bg-gradient-to-b from-red-400 to-orange-300 flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
             alt="MarketLab"
@@ -93,12 +93,8 @@ const RegisterPage = () => {
         <form onSubmit={handleSubmit}>
           <label htmlFor="username" className="block text-sm/6 font-medium text-black-100">
             Username:
-            <span className={validName ? "valid" : "hide"}>
-              <FontAwesomeIcon icon={faCheck}/>
-            </span>
-            <span className={validName || !user ? "hide" : "invalid"}>
-              <FontAwesomeIcon icon={faTimes}/>
-            </span>
+            {(user && validName) && (<FontAwesomeIcon icon={faCheck} />)}
+            {(user && !validName) && (<FontAwesomeIcon icon={faTimes} />)}
           </label>
 
           <input
@@ -112,23 +108,21 @@ const RegisterPage = () => {
             aria-describedby="uidnote"
             onFocus={() => setUserFocus(true)}
             onBlur={() => setUserFocus(false)}
-            className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            className="block w-full rounded-md bg-white/50 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/50 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:text-sm/6"
           />
 
-          <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}> <FontAwesomeIcon icon={faInfoCircle} />
-            4 to 24 characters.<br/>
-            Must begin with a letter. <br/>
-            Letters, number, underscores, hyphens allowed.
-          </p>
+          {(user && !validName) && (
+            <div>
+              <FontAwesomeIcon icon={faInfoCircle} />
+              4 to 24 characters.<br/>
+              Must begin with a letter. <br/>
+              Letters, number, underscores, hyphens allowed.
+            </div>)}
 
           <label htmlFor="password" className="block text-sm/6 font-medium text-black-100">
             Password:
-            <span className={validPwd ? "valid" : "hide"}>
-              <FontAwesomeIcon icon={faCheck}/>
-            </span>
-            <span className={validPwd || !pwd ? "hide" : "invalid"}>
-              <FontAwesomeIcon icon={faTimes}/>
-            </span>
+            {(pwd && validPwd) && (<FontAwesomeIcon icon={faCheck} />)}
+            {(pwd && !validPwd) && (<FontAwesomeIcon icon={faTimes} />)}
           </label>
 
           <input
@@ -140,23 +134,22 @@ const RegisterPage = () => {
             aria-describedby="pwdnote"
             onFocus={() => setPwdFocus(true)}
             onBlur={() => setPwdFocus(false)}
-            className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            className="block w-full rounded-md bg-white/50 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/50 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:text-sm/6"
           />
 
-          <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}> <FontAwesomeIcon icon={faInfoCircle} />
-            8 to 24 characters.<br/>
-            Must inlcude uppercase and lowercase letters, a number and a special character. <br/>
-            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span><span aria-label="percent">%</span>
-          </p>
+          {(pwd && !validPwd) && (
+            <div>
+            <FontAwesomeIcon icon={faInfoCircle} />
+              8 to 24 characters.<br/>
+              Must inlcude uppercase and lowercase letters, a number and a special character. <br/>
+              Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span><span aria-label="percent">%</span>
+            </div>
+            )}
 
           <label htmlFor="confirm_pwd" className="block text-sm/6 font-medium text-black-100">
             Confirm Password:
-            <span className={validMatch && matchPwd ? "valid" : "hide"}>
-              <FontAwesomeIcon icon={faCheck}/>
-            </span>
-            <span className={validMatch || !matchPwd ? "hide" : "invalid"}>
-              <FontAwesomeIcon icon={faTimes}/>
-            </span>
+            {(matchPwd && validMatch) && (<FontAwesomeIcon icon={faCheck} />)}
+            {(matchPwd && !validMatch) && (<FontAwesomeIcon icon={faTimes} />)}
           </label>
 
           <input
@@ -168,18 +161,23 @@ const RegisterPage = () => {
             aria-describedby="confirmnote"
             onFocus={() => setMatchFocus(true)}
             onBlur={() => setMatchFocus(false)}
-            className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            className="block w-full rounded-md bg-white/50 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/50 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:text-sm/6"
           />
 
-          <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}> <FontAwesomeIcon icon={faInfoCircle} />
-            Must match the first password input field.
-          </p>
+          {(matchPwd && !validMatch) && (
+            <div>
+              <FontAwesomeIcon icon={faInfoCircle} />
+              Must match the first password input field.
+            </div>
+          )}
 
-          <button disabled={!validName || !validPwd || !validMatch ? true : false}
-                  className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-          >
-            Sign Up
-          </button>
+          <br/>
+            <button disabled={!validName || !validPwd || !validMatch ? true : false}
+                    className="flex w-full justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-red-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500"
+            >
+              Sign Up
+            </button>
+
         </form>
       </div>
 
@@ -188,7 +186,7 @@ const RegisterPage = () => {
           Already have an account?
         </p>
         {/*add router link*/}
-        <a href='/login' className="font-semibold text-indigo-400 hover:text-indigo-300">
+        <a href='/login' className="font-semibold text-red-500 hover:text-red-400">
           Sign In
         </a>
       </div>
